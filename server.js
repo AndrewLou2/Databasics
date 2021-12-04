9761d7c977189d6fa948386f4e190a8f64d74c84
AndrewLou2 <louandrew123@gmail.com>
2021-12-04 17:43:21 -0500

    updated names

--- server.js
+++ server.js
@@ -5,5 +5,4 @@ const path = require("path");
 const app = express();
 const port = 80;
 
-
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
