bd00722a9465ebea74c869cdbf34a9785fe9fd5d
AndrewLou2 <louandrew123@gmail.com>
2021-12-04 18:59:20 -0500

    fixed stuff

---
+++ client/src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Nav, NavItem, NavLink, Media} from 'reactstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+export default class Example extends React.Component {
+    render() {
+      return (
+        <div>
+
+        </div>
+      );
+    }
+  }
