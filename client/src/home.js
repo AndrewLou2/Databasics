955dbd6c31365dd1a3b2c51e498e42744a39b593
JS6379 <jiyong0416@gmail.com>
2021-12-05 18:21:08 -0500

    Added login as customer function

--- client/src/home.js
+++ client/src/home.js
@@ -23,7 +23,8 @@ export default class Example extends React.Component {
     super(props);
     this.state = {
         username: "",
-        password: ""
+        password: "",
+        customer: false
     }
 }
   handleUserNameChange = event => {
@@ -33,26 +34,29 @@ export default class Example extends React.Component {
   handlePasswordChange = event => {
     this.setState({password: event.target.value});
   }
+  handleCheck = event => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    this.setState({customer: value})
+  }
   //when using form, could not introduce two submits
   login = async e => {
     
-    var username = e.target.username.value;
-    var password = e.target.password.value;
+    alert (this.state.username);
+    alert (this.state.password);
+    alert (this.state.customer);
+    
     var cust = true;
     e.preventDefault();
     const response = await fetch('/db/login', {
       method: 'POST',      
-      body: JSON.stringify({ "user": this.state.username, "password": this.state.password, "customer": cust  }),
-      headers: {
-        'Content-Type': 'applications/json'
-    }
+      body: JSON.stringify({ "user": this.state.username, "password": this.state.password, "customer": this.state.customer  }),
+      
     });
     
     const body = await response.text();
     
     this.setState({ responseToPost: body });
-    alert(this.state.username);    
-    alert(this.state.password);
+    
   };
   signUp = async e => {
     alert("Go to registration page");
@@ -62,7 +66,7 @@ export default class Example extends React.Component {
   render() {
     return (
       <div className="App">
-        <form action="/api/login" method="POST">
+        <form onSubmit ={this.login}>
           <div className="input-group">
             <label htmlFor="username">Username </label>
             <input type="text" name="username" onChange={this.handleUserNameChange} />
@@ -71,7 +75,12 @@ export default class Example extends React.Component {
             <label htmlFor="password">Password </label>
             <input type="text" name="password" onChange={this.handlePasswordChange} />
           </div>
-          <br /><br />
+          <br />
+          <div className="input-group">
+            <label htmlFor="customer">Login as Customer </label>
+            <input type="checkbox" name="customer" onChange={this.handleCheck} />
+          </div>
+          <br />
           <button className="primary">Login</button>
         </form>
         <br />
