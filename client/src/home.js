1ae22e908fbcfd16a60d8338f2e5e35cab86140e
JS6379 <jiyong0416@gmail.com>
2021-12-04 21:55:15 -0500

    Added handlers for login and signup

--- client/src/home.js
+++ client/src/home.js
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 export default class Example extends React.Component {
 
+  //when using form, could not introduce two submits
+  //so use this value to compare to both customer and employee
+  login = e => {
+    alert(e.target.username.value);    
+    alert(e.target.password.value);
+    var username = e.target.username.value;
+    var password = e.target.password.value;
+  };
+  signUp = e => {
+    alert("Go to registration page");
+  };
   
   render() {
     return (
-    <div>
-      Login<br /><br />
-      <div>
-        Username<br />
-        <input type="text" autoComplete="new-password" />
+      <div className="App">
+        <form className="form" onSubmit={this.login}>
+          <div className="input-group">
+            <label htmlFor="username">Username </label>
+            <input type="username" name="username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password </label>
+            <input type="password" name="password" />
+          </div>
+          <br /><br />
+          <button className="primary">Login</button>
+        </form>
+        <br />
+        <button className="secondary" onClick={this.signUp}>
+          Sign Up
+        </button>
       </div>
-      <div style={{ marginTop: 10 }}>
-        Password<br />
-        <input type="password" autoComplete="new-password" />
-      </div>
-      {<><small style={{ color: 'red' }}></small><br /></>}<br />
-      <input type="button" value={'Login'} /><br />
-    </div>
-  );
+    );
   }
 }
