eb555d4c7527a18c61faf712fcf94a7d469f858d
AndrewLou2 <louandrew123@gmail.com>
2021-12-04 20:27:28 -0500

    Update employee.js

--- client/src/employee.js
+++ client/src/employee.js
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  ListGroup,
+  Table,
+  ListGroupItem,
+  ButtonGroup,
+  Badge,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
 
 export default class Example extends React.Component {
   render() {
-    return <h1>Employee Page</h1>;
+    return (
+      <div className="main">
+        <h1>Employee Page</h1>
+        <Form class="2">
+          <Row form>
+            <Col md={6}>
+              <h3 for="Employee">Add a new employee</h3>
+              <Input
+                type="text"
+                name="Playlist"
+                id="exampleEmail"
+                placeholder="Enter employee Name..."
+                // value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
   }
 }
