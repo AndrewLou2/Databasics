e2aa6e632a2d6ab277e778ee40f6a797efc3f228
JS6379 <jiyong0416@gmail.com>
2021-12-04 20:06:24 -0500

    Added login skeleton

--- client/src/home.js
+++ client/src/home.js
@@ -1,7 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
 export default class Example extends React.Component {
+
+  
   render() {
-    return <h1>Home Page</h1>;
+    return (
+    <div>
+      Login<br /><br />
+      <div>
+        Username<br />
+        <input type="text" autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" autoComplete="new-password" />
+      </div>
+      {<><small style={{ color: 'red' }}></small><br /></>}<br />
+      <input type="button" value={'Login'} /><br />
+    </div>
+  );
   }
 }
