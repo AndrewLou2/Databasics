f8694612bbd231aa62922c20881a25ad790d0eb3
JS6379 <jiyong0416@gmail.com>
2021-12-05 17:20:53 -0500

    Tried /db/login, no luck

--- client/src/home.js
+++ client/src/home.js
@@ -1,29 +1,75 @@
 import React, { Component, useState } from "react";
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  ListGroup,
+  Table,
+  ListGroupItem,
+  ButtonGroup,
+  Badge,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
 export default class Example extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        username: "",
+        password: ""
+    }
+}
+  handleUserNameChange = event => {
+    this.setState({username: event.target.value});
+  }
+
+  handlePasswordChange = event => {
+    this.setState({password: event.target.value});
+  }
   //when using form, could not introduce two submits
-  //so use this value to compare to both customer and employee
-  login = e => {
-    alert(e.target.username.value);    
-    alert(e.target.password.value);
+  login = async e => {
+    
     var username = e.target.username.value;
     var password = e.target.password.value;
+    var cust = true;
+    e.preventDefault();
+    const response = await fetch('/db/login', {
+      method: 'POST',      
+      body: JSON.stringify({ "user": this.state.username, "password": this.state.password, "customer": cust  }),
+      headers: {
+        'Content-Type': 'applications/json'
+    }
+    });
+    
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+    alert(this.state.username);    
+    alert(this.state.password);
   };
-  signUp = e => {
+  signUp = async e => {
     alert("Go to registration page");
+    
   };
   
   render() {
     return (
       <div className="App">
-        <form className="form" onSubmit={this.login}>
+        <form action="/api/login" method="POST">
           <div className="input-group">
             <label htmlFor="username">Username </label>
-            <input type="username" name="username" />
+            <input type="text" name="username" onChange={this.handleUserNameChange} />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password </label>
-            <input type="password" name="password" />
+            <input type="text" name="password" onChange={this.handlePasswordChange} />
           </div>
           <br /><br />
           <button className="primary">Login</button>
@@ -31,7 +77,7 @@ export default class Example extends React.Component {
         <br />
         <button className="secondary" onClick={this.signUp}>
           Sign Up
-        </button>
+        </button><br /><br />
       </div>
     );
   }
