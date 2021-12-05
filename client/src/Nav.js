a3ad2e7872378f5cfb3e9c53091f0a79700e3bf2
AndrewLou2 <louandrew123@gmail.com>
2021-12-04 19:39:03 -0500

    added customer page

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Employee from "./employee";
 import Home from "./home";
+import Customer from "./customer";
 
 export default class Example extends React.Component {
   render() {
@@ -16,10 +17,14 @@ export default class Example extends React.Component {
           <NavItem>
             <NavLink href="/employee/">Employee</NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink href="/customer/">Customer</NavLink>
+          </NavItem>
         </Nav>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/employee" element={<Employee />} />
+          <Route path="/customer" element={<Customer />} />
         </Routes>
       </div>
     );
