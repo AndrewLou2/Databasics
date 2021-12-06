254005f2a89aaa3099ba2f6b9cd268eaff2707d6
JS6379 <jiyong0416@gmail.com>
2021-12-06 15:13:05 -0500

    Added list category function

--- client/src/home.js
+++ client/src/home.js
@@ -24,7 +24,12 @@ export default class Example extends React.Component {
     this.state = {
         user: "",
         password: "",
-        customer: false
+        customer: false,
+        category: "",
+        subCategory: "",
+        size: "",
+        gauge: "",
+        rmGroup: ""
     }
 }
   handleUserNameChange = event => {
@@ -34,11 +39,31 @@ export default class Example extends React.Component {
   handlePasswordChange = event => {
     this.setState({password: event.target.value});
   }
+
   handleCheck = event => {
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
     this.setState({customer: value})
   }
-  
+
+  handleCategoryChange = event => {
+    this.setState({category: event.target.value});
+  }
+
+  handleSubCategoryChange = event => {
+    this.setState({subCategory: event.target.value});
+  }
+
+  handleSizeChange = event => {
+    this.setState({size: event.target.value});
+  }
+
+  handleGaugeChange = event => {
+    this.setState({gauge: event.target.value});
+  }
+
+  handleRMGroupChange = event => {
+    this.setState({rmGroup: event.target.value});
+  }  
   
   login = async e => {
     
@@ -57,9 +82,59 @@ export default class Example extends React.Component {
     
     const body = await response.text();
     alert (body);
-    this.setState({ responseToPost: body });
-    
+    this.setState({ responseToPost: body });    
   };
+
+  listCategory = async e => {
+        
+    e.preventDefault();
+    const response = await fetch('/db/listcategory', {
+      method: 'POST',      
+      headers: { 'Content-Type': 'application/json' },   
+      body: JSON.stringify({"filter":{ "category": this.state.category, 
+      "sub_category": "", "size": "", "gauge": "", "material": "", "uom": "", "groupID": ""}}),   
+
+    });
+    
+    const body = await response.text();
+    alert (body);
+    this.setState({ responseToPost: body });    
+  };
+
+  search = async e => {
+        
+    e.preventDefault();
+    const response = await fetch('/db/orderHistory', {
+      method: 'GET',      
+      headers: { 'Content-Type': 'application/json' },      
+
+    });    
+    
+    const body = await response.text();
+    alert (body);
+    this.setState({ responseToPost: body });    
+  };
+
+  placeOrder = async e => {
+    
+    alert (this.state.user);
+    alert (this.state.password);
+    alert (this.state.customer);
+    
+    var cust = true;
+    e.preventDefault();
+    const response = await fetch('/db/login', {
+      method: 'POST',      
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({"user":{ "user": this.state.user, "password": this.state.password, "customer": this.state.customer }}),
+
+    });
+    
+    const body = await response.text();
+    alert (body);
+    this.setState({ responseToPost: body });    
+  };
+
   signUp = async e => {
     alert("Go to registration page");    
     
@@ -100,9 +175,60 @@ export default class Example extends React.Component {
           <button type="submit">Login</button>
         </form>
         <br />
-        <button className="register" onClick={this.signUp}>
+        <button className="register" onClick={this.listCategory}>
           Sign Up
-        </button><br /><br />        
+        </button><br /><br />     
+
+        <h4> Order History by Filter </h4>
+        <form onSubmit ={this.listCategory}>
+          <div className="category">
+            <label>Category: </label>
+            <input 
+              type="text" 
+              value = {this.state.category}
+              onChange={this.handleCategoryChange}  
+            />
+          </div>
+          <br />
+          <div className="subCategory">
+            <label>Sub_Category: </label>
+            <input 
+              type="text" 
+              value = {this.state.subCategory}
+              onChange={this.handleSubCategoryChange} 
+            />
+          </div>
+          <br />
+          <div className="size">
+            <label>Size: </label>
+            <input 
+              type="text" 
+              value = {this.state.size}
+              onChange={this.handleSizeChange}
+            />
+          </div>
+          <br />
+          <div className="gauge">
+            <label>Gauge: </label>
+            <input 
+              type="text" 
+              value = {this.state.gauge}
+              onChange={this.handleGaugeChange}
+            />
+          </div>
+          <br />
+          <div className="rmGroup">
+            <label>RM_Group: </label>
+            <input 
+              type="text" 
+              value = {this.state.rmGroup}
+              onChange={this.handleRMGroupChange}
+            />
+          </div>
+          <br />
+          <button type="submit">Search</button>
+        </form>
+        <br />   
       </div>
     );
   }
