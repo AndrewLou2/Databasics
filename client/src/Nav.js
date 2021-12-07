0542cadb2896d982cda15dae4754cc715f0e2128
AndrewLou2 <alou2@uwo.ca>
2021-12-06 20:13:56 -0500

    implemented quit function

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -6,6 +6,7 @@ import Employee from "./employee";
 import Home from "./home";
 import Customer from "./customer";
 import Register from "./register";
+import Quit from "./quit";
 
 export default class Example extends React.Component {
   render() {
@@ -24,12 +25,16 @@ export default class Example extends React.Component {
           <NavItem>
             <NavLink href="/register/">Register</NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink href="/quit/">Quit</NavLink>
+          </NavItem>
         </Nav>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/employee" element={<Employee />} />
           <Route path="/customer" element={<Customer />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/quit" element={<Quit />} />
         </Routes>
       </div>
     );
